00b9ff6d5c86cda149cb67e485de2232eb017bc3
Arvinispro <arvinyang71@gmail.com>
2026-09-22 21:06:51 -0400

    fix bugs

--- promise.cc
+++ promise.cc
@@ -1,8 +1,7 @@
 import <iostream>
 
 int main() {
-  std::cout << "I promise that I will not use generative AI tools to complete any of my work in this
-course, including code, tests, design documents, or written assignments." << std::endl;
+  std::cout << "I promise that I will not use generative AI tools to complete any of my work in this course, including code, tests, design documents, or written assignments." << std::endl;
 
   return 0
 }
