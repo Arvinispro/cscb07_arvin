bf933d3083de5fee096baff7f3bf7233ff9b24ee
Arvinispro <arvinyang71@gmail.com>
2026-09-22 21:04:37 -0400

    instruction and A0

---
+++ promise.cc
@@ -0,0 +1,8 @@
+import <iostream>
+
+int main() {
+  std::cout << "I promise that I will not use generative AI tools to complete any of my work in this
+course, including code, tests, design documents, or written assignments." << std::endl;
+
+  return 0
+}
